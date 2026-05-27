a2d29642453d8d358bd4c2b1bd11f25adeb021b6
Nathy1234 <nathy-a-oliveira@hotmail.com>
2026-05-27 13:50:07 -0300

    Corrige versao serverless-wsgi e path da funcao

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,6 +1,8 @@
 import sys, os
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+for _p in ['/var/task', os.path.dirname(os.path.abspath(__file__)), os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))]:
+    if _p and _p not in sys.path:
+        sys.path.insert(0, _p)
 
 import serverless_wsgi
 from app import app
